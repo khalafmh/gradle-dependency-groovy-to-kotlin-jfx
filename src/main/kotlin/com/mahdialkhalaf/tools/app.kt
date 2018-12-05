01dc3b2540af7b0260ba81d5eb8f20a7601461d9
Mahdi AlKhalaf <mahdi@mahdialkhalaf.com>
2018-12-05 17:24:25 +0300

    Allow input dependency to be either in the named arguments format or in the colon separated string format.

--- src/main/kotlin/com/mahdialkhalaf/tools/app.kt
+++ src/main/kotlin/com/mahdialkhalaf/tools/app.kt
@@ -1,12 +1,12 @@
 package com.mahdialkhalaf.tools
 
 import com.github.thomasnield.rxkotlinfx.toObservable
+import io.reactivex.rxkotlin.Observables
 import javafx.application.Application
+import javafx.scene.control.RadioButton
 import javafx.scene.control.TextArea
-import tornadofx.fieldset
-import tornadofx.form
-import tornadofx.singleAssign
-import tornadofx.textarea
+import javafx.scene.control.ToggleGroup
+import tornadofx.*
 
 fun main(args: Array<String>) {
     Application.launch(App::class.java, *args)
@@ -16,34 +16,75 @@ class App : tornadofx.App(View::class) {
 }
 
 class View : tornadofx.View("Gradle Groovy to Kotlin Dependency Migrator") {
-    var inputArea by singleAssign<TextArea>()
-    var outputArea by singleAssign<TextArea>()
+    enum class Style {
+        NAMED,
+        COLON_SEPARATED
+    }
+
+    private var inputArea by singleAssign<TextArea>()
+    private var outputArea by singleAssign<TextArea>()
+    private var styleNamed by singleAssign<RadioButton>()
+    private var styleColonSeparated by singleAssign<RadioButton>()
+    private val styleRadioGroup = ToggleGroup()
 
     override val root = form {
         fieldset("Input") { inputArea = textarea { } }
         fieldset("Output") { outputArea = textarea { } }
+        fieldset("Style") {
+            styleNamed = radiobutton("Named", styleRadioGroup) { isSelected = true }
+            styleColonSeparated = radiobutton("Colon Separated", styleRadioGroup)
+        }
     }
 
     init {
-        inputArea.textProperty().toObservable().subscribe { dependencies ->
-            outputArea.text = dependencies.lines().joinToString("\n") { migrate(it) }
+        Observables.combineLatest(
+            inputArea.textProperty().toObservable(),
+            styleRadioGroup.selectedToggleProperty().toObservable()
+        ).subscribe { textAndToggle ->
+            val style = when (textAndToggle.second) {
+                styleNamed -> {
+                    Style.NAMED
+                }
+                styleColonSeparated -> {
+                    Style.COLON_SEPARATED
+                }
+                else -> {
+                    throw IllegalStateException()
+                }
+            }
+            outputArea.text = textAndToggle.first.lines().joinToString("\n") { migrate(it, style) }
         }
     }
 
 
-    private fun migrate(dependency: String): String {
-        val regex = Regex("(?<scope>\\w+) group: '(?<group>.+)', name: '(?<name>.+)', version: '(?<version>.+)'")
-        if (!dependency.matches(regex)) {
+    private fun migrate(dependency: String, style: Style = Style.NAMED): String {
+        val regex =
+            Regex("\\s*(?<configuration>\\w+)\\s+group:\\s+[\"'](?<group>.+)[\"'],\\s+name:\\s+[\"'](?<name>.+)[\"'],\\s+version:\\s+[\"'](?<version>.+)[\"']")
+        val regex2 =
+            Regex("\\s*(?<configuration>\\w+)\\s+(?<quote>[\"'])(?<group>.+):(?<name>.+):(?<version>.+)\\k<quote>")
+        if (!dependency.matches(regex) && !dependency.matches(regex2)) {
             return dependency
         }
-        val values = regex.matchEntire(dependency)?.groups ?: throw IllegalStateException()
-        val scope = when (values["scope"]?.value) {
+        val groups = when {
+            dependency.matches(regex) -> regex.matchEntire(dependency)?.groups ?: throw IllegalStateException()
+            dependency.matches(regex2) -> regex2.matchEntire(dependency)?.groups ?: throw IllegalStateException()
+            else -> throw IllegalStateException()
+        }
+
+        val configuration = when (groups["configuration"]?.value) {
             "compile" -> "implementation"
             "testCompile" -> "testImplementation"
             "api" -> "api"
             "testApi" -> "testApi"
-            else -> values["scope"]?.value
+            else -> groups["configuration"]?.value
         }
-        return "$scope(group = \"${values["group"]?.value}\", name = \"${values["name"]?.value}\", version = \"${values["version"]?.value}\")"
+        return when (style) {
+            Style.NAMED -> {
+                "$configuration(group = \"${groups["group"]?.value}\", name = \"${groups["name"]?.value}\", version = \"${groups["version"]?.value}\")"
+            }
+            Style.COLON_SEPARATED -> {
+                "$configuration(\"${groups["group"]?.value}:${groups["name"]?.value}:${groups["version"]?.value}\")"
+            }
+        }
     }
 }
